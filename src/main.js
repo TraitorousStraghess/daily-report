d227054858097076ca889a18f886ba71421cad5c
masaya <yoma5128@gmail.com>
2026-04-25 17:20:29 +0900

    submit-data追加（リファクタリング）

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
 import { fetchHistoryData } from "./my-modules/fetch-history-data";
+import { submitData } from "./my-modules/submit-data";
 
 // 設定情報
 const firebaseConfig = {
@@ -23,23 +24,6 @@ if (document.getElementById("js-history")) {
   fetchHistoryData(getDocs, collection, db);
 }
 
-// Cloud Firestoreにデータを送信する
-const submitData = async (e) => {
-  e.preventDefault();
-  const formData = new FormData(e.target);
-  
-  try {
-    const docRef = await addDoc(collection(db, "reports"), {
-      date: new Date(),
-      name: formData.get("name"),
-      work: formData.get("work"),
-      comment: formData.get("comment"),
-    });
-    console.log("Document written with ID: ", docRef.id);
-  } catch (e) {
-    console.error("Error adding document: ", e);
-  }
-};
 
 // Cloud Firestoreにデータを送信する
 if (document.getElementById("js-form")) {
